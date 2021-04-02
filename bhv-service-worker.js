4d614617444b79dd2fab5a2473c6cd17d1f144a7
brueckl.hotvolleys@gmx.at <BruecklHotvolleys@users.noreply.github.com>
2021-04-02 13:52:40 +0200

    presence

--- bhv-service-worker.js
+++ bhv-service-worker.js
@@ -3,7 +3,7 @@
 // the names and the files of the cache
 const CACHE = {
   'main': 'bhv-infoapp-main-de1914629d6daa9c52f5cdd8607eff1b',
-  'data': 'bhv-infoapp-data-c9ce9e488625a86862958e5cc3dab3a8',
+  'data': 'bhv-infoapp-data-482f7641ca48c67449ae471d7fbde0dc',
   'system1': 'bhv-infoapp-system1-396e168e59deacc76eaa7b4b1fbf8334',
   'system4': 'bhv-infoapp-system4-cce310d598413509c955f7a3dad58314',
   'system6': 'bhv-infoapp-system6-e0b78a63025a9acaa66928075091f4c6',
